2bdaeba5830276b10b9fa42c988c47405885e384
Marte Løge <marte.loge@gmail.com>
2013-09-22 16:06:57 +0200

    changed logic to print houses within fixed interval

--- www/js/models/level.js
+++ www/js/models/level.js
@@ -9,16 +9,22 @@ var Level = Backbone.Model.extend({
 
 	update: function (dt){
 		var last = this.get("lastTime") + dt;
-		if(last>this.get("frequency")){
+		var freq=this.get("frequency");
+		if(last>freq){
 			var map = this.get("map");
 			var xr = Math.floor(Math.random()*map.get("width"));
 			var yr = Math.floor(Math.random()*map.get("height"));
 			console.log(xr + " " + yr);
+
+
 			map.get("buildings").add(new Building({
 				x: xr,
 				y: yr
 			}));
+
+			this.set("lastTime", last-freq);
+		} else {
+			this.set("lastTime", last);
 		}
-		this.set("lastTime", last);
 	}
 });
