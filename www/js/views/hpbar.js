f881219d2b7e1615d4e7ca0503f9ceae661e09b2
Marte Løge <marte.loge@gmail.com>
2013-10-10 18:01:29 +0200

    Changed healthbar design
    Removed text in html, added border
    to health bar and added changing
    color to the bar depending on the
    healthscore

--- www/js/views/hpbar.js
+++ www/js/views/hpbar.js
@@ -8,7 +8,18 @@ var HpBar = Backbone.View.extend({
 	render: function() {
 		var context = this.el.getContext("2d");
 			r=this.model.get("player").get("health")/PLAYER_MAX_HP;
-		context.fillStyle = "red";
-		context.fillRect(0, 0, this.el.width*r, this.el.height);
+		context.fillStyle = "white";
+		context.fillRect(0, 0, this.el.width*(1-r), this.el.height);
+		if(r>=0.7){
+			context.fillStyle = "green";
+		}
+		else if(r<0.7 && r>=0.3){
+			context.fillStyle = "orange";
+		}
+		else{
+			context.fillStyle = "red";
+		}
+		
+		context.fillRect(this.el.width*(1-r), 0, this.el.width*r, this.el.height);
 	}
 });
