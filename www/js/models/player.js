610fa7e2e61e36da9c3cbdc55a77e721deb4dbe2
Martstol <martstol@gmail.com>
2013-10-11 14:21:10 +0200

    Added a method for lowering player health

--- www/js/models/player.js
+++ www/js/models/player.js
@@ -2,5 +2,10 @@ var Player = Backbone.Model.extend({
   defaults: {
     "health": PLAYER_MAX_HP,
     "money": 599
+  },
+
+  damage: function(dmg) {
+  	var hp=this.get("health")-dmg;
+  	this.set("health", Math.max(hp, 0));
   }
 });
