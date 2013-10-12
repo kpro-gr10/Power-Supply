0a07025a3ac932e7727140fdf5e18b153259b7d8
Martstol <martstol@gmail.com>
2013-10-12 17:28:06 +0200

    Removed pretty much everything

--- www/js/models/powerline.js
+++ www/js/models/powerline.js
@@ -1,15 +1,3 @@
 var PowerLine = Backbone.Model.extend({
-  defaults: {
-    // The starting point (building) of the power line.
-    startBuilding: null,
 
-    // An ordered collection of connecting points (pylons).
-    connectingPoints: new Backbone.Collection(),
-
-    // The terminal point (building) of the power line.
-    terminalBuilding: null,
-
-    // The total cost of building this power line.
-    cost: 0
-  }
 });
