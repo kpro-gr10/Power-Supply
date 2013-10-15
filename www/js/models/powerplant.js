75c591f6673bc9639ca6de2efeaec929c4aea149
Martstol <martstol@gmail.com>
2013-10-15 16:29:54 +0200

    Powerplant upgrading

--- www/js/models/powerplant.js
+++ www/js/models/powerplant.js
@@ -3,7 +3,19 @@ var Powerplant = Entity.extend({
     sprite: imgLib.factory1,
     x: 0,
     y: 0,
-    level: 1,
-    type: BuildingType.PowerPlant,
+    level: 0,
+    type: BuildingType.Powerplant,
+  },
+
+  canBeUpgraded: function() {
+  	return this.get("level")+1 < POWERPLANT_MAX_LEVEL;
+  },
+
+  getMaxPower: function() {
+  	return POWERPLANT_POWER[this.get("level")];
+  },
+
+  upgrade: function() {
+  	this.set({ level: this.get("level") + 1 });
   }
 });
