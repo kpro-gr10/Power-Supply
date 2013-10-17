d2a9ef5451c6163c9820a1eebd2334ec46b15ab4
Martstol <martstol@gmail.com>
2013-10-17 13:52:23 +0200

    Merges

--- www/js/models/powerline.js
+++ www/js/models/powerline.js
@@ -3,5 +3,19 @@ var PowerLine = Backbone.Model.extend({
     // The buildings connected by this power line.
     buildingA: null,
     buildingB: null,
+  },
+
+  drawPath: function(context, screenX, screenY) {
+    var a = this.get("buildingA"),
+        b = this.get("buildingB");
+
+    context.beginPath();
+    context.lineWidth = POWERLINE_WIDTH;
+    context.strokeStyle = "white";
+    context.moveTo(a.get("x") + a.get("sprite").width/2 - screenX,
+                   a.get("y") + a.get("sprite").height/2 - screenY);
+    context.lineTo(b.get("x") + b.get("sprite").width/2 - screenX,
+                   b.get("y") + b.get("sprite").height/2 - screenY);
+    context.closePath();
   }
 });
