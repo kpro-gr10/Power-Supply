5898670b7d291e9802b4a33f6f39e87403559aa7
Martstol <martstol@gmail.com>
2013-10-30 17:46:51 +0100

    Cleaned up rendering code
    Moved code that renders buildings, powerplants and powerlines to their own respective class. The code for rendering a building is now in building.js etc.

--- www/js/models/powerline.js
+++ www/js/models/powerline.js
@@ -30,5 +30,40 @@ var PowerLine = Backbone.Model.extend({
     this.get("buildingB").connectTo(this);
 
     this.set({state: PowerLineState.Healthy});
+  },
+
+  render: function(context, xPos, yPos) {
+    var state = this.get("state"),
+      a=this.get("buildingA"),
+      b=this.get("buildingB"),
+      x0=a.get("x") + a.get("sprite").width/2 - xPos,
+      y0=a.get("y") + a.get("sprite").height/2 - yPos,
+      x1=b.get("x") + b.get("sprite").width/2 - xPos,
+      y1=b.get("y") + b.get("sprite").height/2 - yPos;
+
+    context.beginPath();
+    context.lineWidth = POWERLINE_WIDTH;
+    context.strokeStyle="black";
+    context.moveTo(x0, y0);
+    context.lineTo(x1, y1);
+    context.stroke();
+    if(state === PowerLineState.Healthy &&
+       (a.get("receivePower") && b.get("receivePower") ||
+       a instanceof Powerplant && b.get("receivePower") ||
+       a.get("receivePower") && b instanceof Powerplant)) {
+      context.beginPath();
+      context.lineWidth = POWERLINE_WIDTH/2;
+      context.strokeStyle="yellow";
+      context.moveTo(x0, y0);
+      context.lineTo(x1, y1);
+      context.stroke();
+    } else if (state === PowerLineState.Broken) {
+      context.beginPath();
+      context.lineWidth = POWERLINE_WIDTH/2;
+      context.strokeStyle="red";
+      context.moveTo(x0, y0);
+      context.lineTo(x1, y1);
+      context.stroke();
+    }
   }
 });
